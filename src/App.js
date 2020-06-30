9c800c0fa9f97b639c8c8d33aad953eb3ab3a0c5
meraj <merajulnu@gmail.com>
2020-06-30 14:27:52 +0600

    Shop page created with Collection Preview and Collection Item components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import {Route} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-function HatsPage() {
-  return <h1>Hats Page</h1>
-}
+
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
